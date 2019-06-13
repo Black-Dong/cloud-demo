badae5aa64994c0c18f94b128b42c08e4e46e551
Black-Dong <1763424201@qq.com>
2019-06-13 17:12:59 +0800

    Eureka的简单使用(Eureka服务端自己也需要注册)

--- consumer-demo/src/main/java/cn/imust/consumer/web/ConsumerController.java
+++ consumer-demo/src/main/java/cn/imust/consumer/web/ConsumerController.java
@@ -2,11 +2,15 @@ package cn.imust.consumer.web;
 
 import cn.imust.consumer.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/consumer")
@@ -15,9 +19,18 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("{id}")
     public User queryById(@PathVariable("id") Long id){
-        String url = "http://localhost:8081/user/" + id;
+
+        //根据服务id获取实例列表
+        List<ServiceInstance> instances = discoveryClient.getInstances("USER-SERVICE");
+        //从实例列表中取出具体实例的ip和端口
+        ServiceInstance serviceInstance = instances.get(0);
+        String url = "http://"+serviceInstance.getHost() + ":" + serviceInstance.getPort()+"/user/" + id;
+
         User user = restTemplate.getForObject(url,User.class);
 
         return user;
--- user-service/src/main/java/cn/imust/user/UserApplication.java
+++ user-service/src/main/java/cn/imust/user/UserApplication.java
@@ -2,8 +2,11 @@ package cn.imust.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
+
+@EnableDiscoveryClient  //客户端注册（兼容三种）
 @SpringBootApplication
 @MapperScan("cn.imust.user.mapper")
 public class UserApplication {
